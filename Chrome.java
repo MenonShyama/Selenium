cdc79ad8da321bbb0bf73c6ea98787438142ce26
MenonShyama <96658329+MenonShyama@users.noreply.github.com>
2026-01-07 20:36:59 +0530

    week1.day2

---
+++ Chrome.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * a) Create a class named Chrome with a main method. 
+		 * b) Inside the main method,
+		 * declare and initialize the following variables: 
+		 * 1. version of type float with
+		 * a value of 91.0f 
+		 * 2. developer of type Stringwith a value of "Google" 
+		 * 3. * isBeta of type booleanwith a value of false 
+		 * 4. releaseYear of type int with a
+		 * value of 2008 
+		 * 5. shortcutKey of type charwith a value of 'C' c) Print the
+		 * values of all variables using System.out.println. d) Run the program and
+		 * confirm that the variable values are printed in the console.
+		 */
+		
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println(version +"\n"+developer +"\n"+isBeta +"\n"+releaseYear +"\n"+shortcutKey);
+
+	}
+
+}
